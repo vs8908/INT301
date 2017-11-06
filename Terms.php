aa66aa81910bcbc902ca122f38aa6c2d2314f746
vs8908 <33391723+vs8908@users.noreply.github.com>
2017-11-07 00:11:18 +0530

    Add files via upload

---
+++ Terms.php
@@ -0,0 +1,45 @@
+<?php
+    echo "";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Validation Project</title>
+
+    <link href="css/Style.css" rel="stylesheet">
+</head>
+<body style="background: #99315e; background-image:url(pexels-photo-128458.jpeg); opacity: 1; z-index:2;" ">
+    <div class="container">
+        <div class="row">
+    <div class="col-sm-12" style="margin-top:40px;">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-6" style="border-radius: 4px; background-color:white; margin-top:40px;">
+                    <br />
+                    <label>Terms and Conditions</label>
+                    <br /><br />
+                    <section class=".dl-horizontal">
+                    <paragraph>
+                    By registering here and creating your "Telegraph.co.uk Profile", you can access different services that are offered by us without having to register for each service separately. If a service you wish to subscribe to has additional terms and conditions, you will be asked to accept these separately.
+                    <br /><br />
+                    1.2
+                    You agree that:
+                    <br />(1)
+                    you will keep your username and password safe, and won't share them with anyone.
+                    <br />(2)
+                    you will not pass yourself off as someone else or create multiple, false accounts.
+                    </paragraph>
+                    </section>
+
+                    <br /><br />
+                    <a href="http://localhost/phpproject/ValidateForm.php"><button class="btn btn-primary">Go Back</button></a>
+                <br /><br />
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
